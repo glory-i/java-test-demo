4ac0668d4d46a864e34c350d87982c154c779734
glory <glory.iweriebor@qucoon.com>
2025-07-30 16:38:52 +0100

    initial commit test

--- src/main/java/qucoon/mod/SpringServerless/QucoonSpringServerlessApplication.java
+++ src/main/java/qucoon/mod/SpringServerless/QucoonSpringServerlessApplication.java
@@ -1,5 +1,6 @@
 package qucoon.mod.SpringServerless;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -9,6 +10,18 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class QucoonSpringServerlessApplication {
 
 	public static void main(String[] args) {
+
+		// Load .env
+		Dotenv dotenv = Dotenv.load();
+
+		// Set as system properties for Spring Boot to read
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("TRUST_SERVER_CERTIFICATE", dotenv.get("TRUST_SERVER_CERTIFICATE"));
+		System.setProperty("DATABASE", dotenv.get("DATABASE"));
+		System.setProperty("ENCRYPT", dotenv.get("ENCRYPT"));
+
 		SpringApplication.run(QucoonSpringServerlessApplication.class, args);
 	}
 
