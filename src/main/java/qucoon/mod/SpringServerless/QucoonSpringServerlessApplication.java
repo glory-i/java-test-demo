ff0a00fae2e4f944ce8d23f2fcc94f0409f99f3a
glory <glory.iweriebor@qucoon.com>
2025-07-30 18:38:29 +0100

    pipeline setup test fix

--- src/main/java/qucoon/mod/SpringServerless/QucoonSpringServerlessApplication.java
+++ src/main/java/qucoon/mod/SpringServerless/QucoonSpringServerlessApplication.java
@@ -18,9 +18,10 @@ public class QucoonSpringServerlessApplication {
 		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("TRUST_SERVER_CERTIFICATE", dotenv.get("TRUST_SERVER_CERTIFICATE"));
-		System.setProperty("DATABASE", dotenv.get("DATABASE"));
-		System.setProperty("ENCRYPT", dotenv.get("ENCRYPT"));
+
+//		System.setProperty("TRUST_SERVER_CERTIFICATE", dotenv.get("TRUST_SERVER_CERTIFICATE"));
+//		System.setProperty("DATABASE", dotenv.get("DATABASE"));
+//		System.setProperty("ENCRYPT", dotenv.get("ENCRYPT"));
 
 		SpringApplication.run(QucoonSpringServerlessApplication.class, args);
 	}
